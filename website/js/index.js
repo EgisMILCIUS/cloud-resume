edd8b64250ba133e97d2688d0c1bcbb074dc5d18
EgisMILCIUS <164767916+EgisMILCIUS@users.noreply.github.com>
2024-04-07 17:13:53 +0200

    deleted

--- website/js/index.js
+++
@@ -1,8 +0,0 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch("https://wdfrp5fhtuglltryd76bex7zyq0epmgs.lambda-url.us-east-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = ` This page has ${data} Views!`;
-}
-
-updateCounter();
